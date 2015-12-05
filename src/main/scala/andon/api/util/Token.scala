bdb8e1fce94ba373e2866effc889ae365881f1db
amutake <amutake.s@gmail.com>
2015-12-06 00:26:04 +0900

    Use application.conf to specify secret-key and algorithm

--- src/main/scala/andon/api/util/Token.scala
+++ src/main/scala/andon/api/util/Token.scala
@@ -1,15 +1,23 @@
 package andon.api.util
 
+import com.typesafe.config.ConfigFactory
 import org.json4s._
-import org.json4s.jackson.Serialization.{read, write}
+import org.json4s.jackson.Serialization.write
 import pdi.jwt.{ JwtJson4s, JwtAlgorithm }
+import pdi.jwt.algorithms.JwtHmacAlgorithm
 
 final case class Token(userId: Long, username: String, expirationDate: Long)
 
 object Token {
 
-  val key = "secret" // FIXME
-  val algo = JwtAlgorithm.HS256
+  private val conf = ConfigFactory.load()
+  private val key = conf.getString("jwt.secretKey")
+  private val algo: JwtHmacAlgorithm = {
+    JwtAlgorithm.optionFromString(conf.getString("jwt.algorithm")) match {
+      case Some(algo: JwtHmacAlgorithm) => algo
+      case _ => throw new ClassCastException("config `jwt.algorithm` must be JwtHmacAlgorithm")
+    }
+  }
 
   implicit val formats = DefaultFormats
 
