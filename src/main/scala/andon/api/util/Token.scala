b75e7448e3c409431252230453b390a5055d8a42
amutake <amutake.s@gmail.com>
2015-12-18 20:02:58 +0900

    Add allowedEach function

--- src/main/scala/andon/api/util/Token.scala
+++ src/main/scala/andon/api/util/Token.scala
@@ -23,33 +23,29 @@ final case class Token(
     partition(rights, _ => Forbidden(NoPermission()), f)
   }
 
+  // if there are some matched rights, the first one is invoked.
+  def allowedEach[A](rightFuncs: (Right, User => Output[A])*)(implicit s: DBSession) = {
+    withUser { user =>
+      rightFuncs
+        .find(rf => Right.has(user, rf._1))
+        .map(rf => rf._2(user))
+        .getOrElse(Forbidden(NoPermission()))
+    }
+  }
+
+  private def withUser[A](f: User => Output[A])(implicit s: DBSession) = {
+    User.find(userId).map(f)
+      .getOrElse(NotFound(ResourceNotFound("You are a deleted user.")))
+  }
   private def partition[A](
     rights: Seq[Right],
     matched: User => Output[A],
     unmatched: User => Output[A]
   )(implicit s: DBSession): Output[A] = {
-    User.find(userId).map { user =>
-      val b = rights.exists {
-        case Right.Admin => user.admin
-        case Right.Suspended => user.suspended
-        case Right.CohortOf(t) => user.times == t
-        case Right.ClassmateOf(t, 1, c) =>
-          user.times == t && user.classFirst == Some(c)
-        case Right.ClassmateOf(t, 2, c) =>
-          user.times == t && user.classSecond == Some(c)
-        case Right.ClassmateOf(t, 3, c) =>
-          user.times == t && user.classThird == Some(c)
-        case Right.ClassmateOf(_, _, _) => false
-        case Right.ChiefOf(t, 1, c) =>
-          user.times == t && user.classFirst == Some(c) && user.chiefFirst == Some(true)
-        case Right.ChiefOf(t, 2, c) =>
-          user.times == t && user.classSecond == Some(c) && user.chiefSecond == Some(true)
-        case Right.ChiefOf(t, 3, c) =>
-          user.times == t && user.classThird == Some(c) && user.chiefThird == Some(true)
-        case Right.ChiefOf(_, _, _) => false
-      }
+    withUser { user =>
+      val b = rights.exists(Right.has(user, _))
       if (b) matched(user) else unmatched(user)
-    }.getOrElse(NotFound(ResourceNotFound("You are a deleted user.")))
+    }
   }
 }
 
@@ -62,6 +58,26 @@ object Right {
   case class ClassmateOf(times: Short, grade: Short, `class`: Short) extends Right
   // if chief then classmate
   case class ChiefOf(times: Short, grade: Short, `class`: Short) extends Right
+
+  def has(user: User, right: Right): Boolean = right match {
+    case Right.Admin => user.admin
+    case Right.Suspended => user.suspended
+    case Right.CohortOf(t) => user.times == t
+    case Right.ClassmateOf(t, 1, c) =>
+      user.times == t && user.classFirst == Some(c)
+    case Right.ClassmateOf(t, 2, c) =>
+      user.times == t && user.classSecond == Some(c)
+    case Right.ClassmateOf(t, 3, c) =>
+      user.times == t && user.classThird == Some(c)
+    case Right.ClassmateOf(_, _, _) => false
+    case Right.ChiefOf(t, 1, c) =>
+      user.times == t && user.classFirst == Some(c) && user.chiefFirst == Some(true)
+    case Right.ChiefOf(t, 2, c) =>
+      user.times == t && user.classSecond == Some(c) && user.chiefSecond == Some(true)
+    case Right.ChiefOf(t, 3, c) =>
+      user.times == t && user.classThird == Some(c) && user.chiefThird == Some(true)
+    case Right.ChiefOf(_, _, _) => false
+  }
 }
 
 object Token {
